b63b43746979380ff052e0b7c7c3e75bd2f2bb51
unknown <aavik743@gmail.com>
2022-07-06 13:38:27 +0530

    [Abhik]: Refactored views for cart app

--- carts/views.py
+++ carts/views.py
@@ -16,42 +16,20 @@ class AddToCartAPI(APIView):
     def post(self, request, user_id):
         try:
             user = User.objects.filter(pk=user_id).first()
-            print(user)
             if user:
                 cart_serializer = AddToCartSerializer(data=request.data)
                 cart_serializer.is_valid()
+
                 book_quantity = cart_serializer.validated_data.get('book_quantity')
                 book_id = cart_serializer.validated_data.get('book_id')
+
                 book = Book.objects.get(pk=book_id)
-                if book:
-                    if book.book_quantity >= book_quantity:
-                        cart = Cart.objects.filter(book_id=book_id).first()
-                        print(cart.user)
-                        if cart.user == user:
-                            if cart.book == book:
-                                cart.book_quantity += book_quantity
-                                book.book_quantity -= book_quantity
-                                cart.total_price = cart.book_quantity * book.price
-                                cart.save()
-                                book.save()
-                                return Response(
-                                    {'message': 'cart updated', 'status_code': 200, 'data': cart_serializer.data})
-                            book.book_quantity -= book_quantity
-                            total_price = book.price * book_quantity
+                cart = Cart.objects.filter(book_id=book_id).first()
 
-                            cart = Cart.objects.create(user=user, book=book,
-                                                       book_quantity=book_quantity, total_price=total_price)
-                            cart.save()
-                            book.save()
-                            return Response({'message': 'cart added', 'status_code': 200, 'data': cart_serializer.data})
-                        book.book_quantity -= book_quantity
-                        total_price = book.price * book_quantity
+                Cart.add_to_cart_validation(user, book, cart, book_quantity)
 
-                        cart = Cart.objects.create(user=user, book=book,
-                                                   book_quantity=book_quantity, total_price=total_price)
-                        cart.save()
-                        book.save()
-                        return Response({'message': 'cart added', 'status_code': 200, 'data': cart_serializer.data})
+                return Response({'message': 'cart added', 'status_code': 200, 'data': cart_serializer.data})
+
         except Exception as e:
             logger.logging.error('Log Error Message')
             return Response({'message': str(e), 'status_code': 400})
@@ -63,11 +41,15 @@ class CartAPI(APIView):
     def get(self, request, user_id):
         try:
             user = User.objects.filter(pk=user_id).first()
+
             if user:
                 cart = Cart.objects.filter(user=user)
+
                 if cart:
                     cart_serializer = GetCartSerializer(cart, many=True)
+
                     return Response({'message': 'cart fetched', 'status_code': 200, 'data': cart_serializer.data})
+
         except Exception as e:
             logger.logging.error('Log Error Message')
             return Response({'message': str(e), 'status_code': 400})
@@ -77,37 +59,33 @@ class UpdateCartAPI(APIView):
 
     @method_decorator(token_required)
     def patch(self, request, user_id):
-        user = User.objects.filter(pk=user_id).first()
-        if user:
-            cart = Cart.objects.filter(user=user).first()
-            book = Book.objects.filter(pk=cart.book_id).first()
-            if book:
-                if cart:
-                    book_quantity = int(request.data.get('book_quantity'))
+        try:
+            user = User.objects.filter(pk=user_id).first()
 
-                    if cart.book_quantity > book_quantity:
-                        count = cart.book_quantity - book_quantity
+            if user:
+                data_ = Cart.update_cart_validation(user, request)
+                data = {'book quantity in cart': data_}
 
-                        book.book_quantity += count
-
-                    if cart.book_quantity < book_quantity:
-                        count = book_quantity - cart.book_quantity
-
-                        book.book_quantity -= count
-
-                    cart.book_quantity = book_quantity
-                    cart.save()
-                    book.save()
-                    data = {'book quantity in cart': cart.book_quantity}
-                    return Response({'message': 'cart updated', 'status_code': 200, 'data': data})
+                return Response({'message': 'cart updated', 'status_code': 200, 'data': data})
+        except Exception as e:
+            logger.logging.error('Log Error Message')
+            return Response({'message': str(e), 'status_code': 400})
 
     @method_decorator(token_required)
     def delete(self, request, user_id):
-        user = User.objects.filter(pk=user_id).first()
-        if user:
-            cart = Cart.objects.filter(user=user).first()
-            book = Book.objects.filter(pk=cart.book_id).first()
-            book.book_quantity += cart.book_quantity
-            cart.delete()
-            book.save()
-            return Response({'message': 'cart removed', 'status_code': 200})
+        try:
+            user = User.objects.filter(pk=user_id).first()
+
+            if user:
+                cart = Cart.objects.filter(user=user).first()
+                book = Book.objects.filter(pk=cart.book_id).first()
+                
+                book.book_quantity += cart.book_quantity
+
+                cart.delete()
+                book.save()
+
+                return Response({'message': 'cart removed', 'status_code': 200})
+        except Exception as e:
+            logger.logging.error('Log Error Message')
+            return Response({'message': str(e), 'status_code': 400})
